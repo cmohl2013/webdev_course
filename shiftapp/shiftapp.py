b1b36fbb7d6ca4fd6dad589abc42a8788529d435
moehlc <christoph.moehl@dzne.de>
2015-01-11 00:49:33 +0100

    worked on unit 2 qiz 2

--- shiftapp/shiftapp.py
+++ shiftapp/shiftapp.py
@@ -1,7 +1,34 @@
 import cgi
 import webapp2
+import re
+from jinja2 import Environment, PackageLoader
 
 abc = 'abcdefghijklmnopqrstuvwxyz'
+
+
+
+def isValidInput(username, regex):
+    USER_RE = re.compile(regex)
+    if USER_RE.match(username):
+        return True
+    return False    
+
+def isValidUsername(username):
+    return isValidInput(username, "^[a-zA-Z0-9_-]{3,20}$")
+
+def isValidPw(pw):
+    return isValidInput(pw,  "^.{3,20}$")
+
+def isValidEmail(email):
+    if email == '':
+        return True
+    return isValidInput(email,  "^[\S]+@[\S]+\.[\S]+$")    
+
+
+def isValidSignup(username, pw, verify, email):
+    if isValidUsername(username) and isValidPw(pw) and (pw == verify) and isValidEmail(email):
+        return True
+    return False    
 
 def calcIndex(idx):
     if idx >= 26:
@@ -44,7 +71,7 @@ def renderForm(text=''):
     ''' % text
     return form
 
-class MainPage(webapp2.RequestHandler):
+class Rotation(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
         self.response.write(renderForm())
@@ -52,6 +79,63 @@ class MainPage(webapp2.RequestHandler):
         rotated = shiftSent(self.request.get('text'))
         self.response.write(renderForm(text=rotated))    
 
+
+class BasicInputHandler(webapp2.RequestHandler):
+    def get(self):
+        env = Environment(loader=PackageLoader('shiftapp', 'templates'))
+        template = env.get_template('basicsignup.html')
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(template.render(\
+            username = ''\
+            , pw = ''\
+            ,verify = ''\
+            ,email = ''\
+            , username_errormsg=''\
+            , pw_errormsg=''\
+            , pw_verify_errormsg=''\
+            , email_errormsg=''
+            ))
+    def post(self):
+        username = self.request.get('username')    
+        pw = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+
+        if isValidSignup(username, pw, verify, email):
+            self.redirect('/unit2/welcome?username=' + username)
+        else:
+            username_errormsg = ''
+            email_errormsg = ''
+            pw_verify_errormsg = ''
+            pw_errormsg = ''
+
+            if not isValidUsername(username):
+                username_errormsg = "That's not a valid username."
+            if not isValidEmail(email):
+                email_errormsg = "That's not a valid email."
+            if pw != verify:
+                pw_verify_errormsg = "Your passwords didn't match."
+            if not isValidPw(pw):
+                pw_errormsg = "That wasn't a valid password."       
+                    
+            env = Environment(loader=PackageLoader('shiftapp', 'templates'))
+            template = env.get_template('basicsignup.html')
+
+            self.response.write(template.render(\
+            username = username\
+            , pw = pw\
+            ,verify = verify\
+            ,email = email\
+            , username_errormsg=username_errormsg\
+            , pw_errormsg=pw_errormsg\
+            , pw_verify_errormsg=pw_verify_errormsg\
+            , email_errormsg=email_errormsg
+            ))
+                    
+
+class SignupSuccessHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Welcome, ' + self.request.get('username'))
 # class TestHandler(webapp2.RequestHandler):
 #     def post(self):
 #         q = self.request.get('q')
@@ -60,6 +144,7 @@ class MainPage(webapp2.RequestHandler):
 #         self.response.out.write(self.request)
 
 application = webapp2.WSGIApplication([
-    ('/', MainPage),
-    
+    ('/unit2/rot13', Rotation),
+    ('/unit2/basicinput', BasicInputHandler),
+    ('/unit2/welcome', SignupSuccessHandler)
 ], debug=True)  
